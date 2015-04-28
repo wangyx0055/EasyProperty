4a0cbfab7b2c2c57216b1e55443859625021d333
applenele <1941973283@qq.com>
2015-04-28 11:50:03 +0800

    架构

---
+++ EasyProperty/EasyProperty.Entity/Place.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyProperty.Entity
+{
+    /// <summary>
+    /// 会所管理
+    /// </summary>
+    [Table("T_Place")]
+    public class Place
+    {
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 会所名称
+        /// </summary>
+        public string PlaceName { get; set; }
+
+        /// <summary>
+        /// 会所地址
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        ///   增加时间
+        /// </summary>
+        public DateTime Time { get; set; }
+    }
+}
---
+++ EasyProperty/EasyProperty.Entity/Project.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyProperty.Entity
+{
+    /// <summary>
+    /// 招商项目
+    /// </summary>
+    [Table("T_Project")]
+    public class Project
+    {
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        public string ProjectName { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 客户ID
+        /// </summary>
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        /// <summary>
+        /// 增加时间
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public DateTime Begin { get; set; }
+
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public DateTime End { get; set; }
+    }
+}
